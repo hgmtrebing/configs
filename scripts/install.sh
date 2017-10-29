fe85da14451de37ec1fecd52b24409f836744795
Harry Geoffrey Trebing <hgmtrebing@outlook.com>
2017-10-29 15:28:46 -0400

    Addition of install.sh, which should be run to set up the machine for the first time

---
+++ scripts/install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+apt-get install -yq vim
+apt-get install -yq git
+apt-get install -yq i3
+apt-get install -yq herbstluftwm
+apt-get install -yq openbox
+apt-get install -yq bspwm
+apt-get install -yq compton
+apt-get install -yq conky
+apt-get install -yq wget
+apt-get install -yq volti
+apt-get install -yq pavucontrol
+apt-get install -yq feh
+
+apt-get install -yq terminator
+apt-get install -yq wireshark
+apt-get install -yq nmap
+
+apt-get install -yq firefox
+apt-get install -yq chromium
+apt-get install -yq thunderbird
+apt-get install -yq weechat
+apt-get install -yq pidgin
+apt-get install -yq hexchat
+
+apt-get install -yq focuswriter
+apt-get install -yq libreoffice
+
+apt-get install -yq python2.7
+apt-get install -yq python3.5
+apt-get install -yq pip
+apt-get install -yq geany
+
+add-apt-repository ppa:numix/ppa
+apt-get update
+apt-get install numix-icon-theme
+apt-get install numix-icon-theme-circle
